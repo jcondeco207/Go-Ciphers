7fa900e9d17209c5b06f98a523cd7ada769326eb
jcondeco207 <joao.r.condeco@gmail.com>
2023-10-19 23:53:45 +0100

    Xor Cipher

--- examples/examples.go
+++ examples/examples.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"jcondeco-ciphers.com/affine"
+	"jcondeco-ciphers.com/xor"
 )
 
 func main() {
@@ -13,4 +14,12 @@ func main() {
 	// Affine example
 	fmt.Println("Before encryption: " + original)
 	fmt.Println("After encryption: " + affine.Encrypt(original, 11, 17, false)) // Set true if you want spaces as well
+
+	// Xor example
+	original = "Hello, XOR Cipher!"
+
+	fmt.Println("Before encryption: " + original)
+	xorCipher := xor.XorString(original, byte(0x7F))
+	fmt.Println("After encryption: " + xorCipher)
+	fmt.Println("Back to normal: " + xor.XorString(xorCipher, byte(0x7F)))
 }
--- xor/xor.go
+++ xor/xor.go
@@ -1,1 +1,12 @@
 package xor
+
+func XorString(text string, key byte) string {
+
+	encrypted := make([]byte, len(text))
+
+	for i := 0; i < len(text); i++ {
+		encrypted[i] = text[i] ^ key
+	}
+
+	return string(encrypted)
+}
